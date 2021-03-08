3ebe7b06f969358a017d1a29888ff3ee4fdd54c2
Alex Curran <alexcurran.va@gmail.com>
2021-03-08 16:28:33 -0600

    Play/pause video events working

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateBtn(event) {
+    const icon = event.target.paused ? "►" : "❚ ❚";
+    
+    toggle.textContent = icon;
+}
+
+// Event listeners
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateBtn);
+video.addEventListener("pause", updateBtn);
+
+toggle.addEventListener("click", togglePlay);
